904c75ae214052f287e149cfe3376b425d846a99
duonghx21 <55440227+duonghx21@users.noreply.github.com>
2020-08-14 03:20:30 +0700

    - sua mon an - xoa mon anasdasd

--- app/src/main/java/com/example/epapp_demo/fragment/PhanloaiFragment.java
+++ app/src/main/java/com/example/epapp_demo/fragment/PhanloaiFragment.java
@@ -8,40 +8,32 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-
-
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.epapp_demo.DAO.PhanLoaiDAO;
+import com.example.epapp_demo.DAO.CuaHangDAO;
 import com.example.epapp_demo.R;
-import com.example.epapp_demo.adapter.PhanLoaiAdapter;
-import com.example.epapp_demo.model.PhanLoai;
-
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
+import com.example.epapp_demo.adapter.CuaHangAdapter;
+import com.example.epapp_demo.model.CuaHang;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
-public class PhanloaiFragment extends Fragment {
+public class PhanLoaiFragment extends Fragment {
 
-    PhanLoaiDAO phanLoaiDAO= new PhanLoaiDAO(getActivity());
-    private FirebaseAuth mAuth;
-    DatabaseReference mData = FirebaseDatabase.getInstance().getReference("PhanLoai");
-    String LoaiID;
-    public static PhanLoaiAdapter phanLoaiAdapter;
-    RecyclerView lv;
-    ArrayList<PhanLoai> list = new ArrayList<>();
-    FloatingActionButton add;
 
-    public PhanloaiFragment() {
+    public PhanLoaiFragment() {
         // Required empty public constructor
     }
 
@@ -50,49 +42,6 @@ public class PhanloaiFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_phanloai, container, false);
-        lv = view.findViewById(R.id.rcvPhanLoai);
-        add = view.findViewById(R.id.btnAddPhanLoai);
-        mAuth = FirebaseAuth.getInstance();
-
-        list = phanLoaiDAO.getAll();
-        phanLoaiAdapter= new PhanLoaiAdapter(list,getActivity());
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        lv.setLayoutManager(layoutManager);
-        lv.setAdapter(phanLoaiAdapter);
-        Log.d("test2", String.valueOf(list));
-
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                View view1 = getLayoutInflater().inflate(R.layout.add_loai,null);
-                final EditText tenloai = view1.findViewById(R.id.edttenlaoi);
-                final EditText motalaoi = view1.findViewById(R.id.edtMota);
-                builder.setView(view1);
-                builder.setPositiveButton("Thêm", new DialogInterface.OnClickListener() {
-                    @SuppressLint("RestrictedApi")
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                        String PhanLoaiID = mData.push().getKey();
-                        String MaSach = mData.child(PhanLoaiID).push().getKey();
-
-                        String tenl1 = tenloai.getText().toString();
-                        String motal1 = motalaoi.getText().toString();
-                        PhanLoai s = new PhanLoai(MaSach,tenl1,motal1);
-                        phanLoaiDAO.insert(s);
-
-                    }
-                }).setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-                builder.setView(view1);
-                builder.show();
-            }
-        });
         return view;
     }
 }
