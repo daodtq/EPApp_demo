434c89e682b0db6e315da0613a88b3ab1c60d5d6
DaoDTQ-FD <trong.quang.dao@gmail.com>
2020-08-14 22:55:27 +0700

    Push all

--- app/src/main/java/com/example/epapp_demo/fragment/PhanloaiFragment.java
+++ app/src/main/java/com/example/epapp_demo/fragment/PhanloaiFragment.java
@@ -4,16 +4,15 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.epapp_demo.DAO.PhanLoaiDAO;
 import com.example.epapp_demo.R;
@@ -26,11 +25,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * create an instance of this fragment.
- */
-public class PhanloaiFragment extends Fragment {
+public class PhanLoaiFragment extends Fragment {
 
     PhanLoaiDAO phanLoaiDAO= new PhanLoaiDAO(getActivity());
     private FirebaseAuth mAuth;
@@ -41,7 +36,7 @@ public class PhanloaiFragment extends Fragment {
     ArrayList<PhanLoai> list = new ArrayList<>();
     FloatingActionButton add;
 
-    public PhanloaiFragment() {
+    public PhanLoaiFragment() {
         // Required empty public constructor
     }
 
